75934241b2c4dd629713413c97ee463cc4cf2464
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-09-14 08:44:21 -0600

    Just starting

---
+++ DIGIT STRIPPING/src/DIGIT_STRIPPING_CHALLENGES.java
@@ -0,0 +1,39 @@
+
+public class DIGIT_STRIPPING_CHALLENGES
+	{
+
+		public static void main(String[] args)
+			{
+				challengeOne();
+				challengeTwo();
+				challengeThree();
+				challengeFour();
+				challengeFive();
+			}
+
+		private static void challengeOne()
+			{
+				
+			}
+
+		private static void challengeTwo()
+			{
+				
+			}
+
+		private static void challengeThree()
+			{
+				
+			}
+
+		private static void challengeFour()
+			{
+				
+			}
+
+		private static void challengeFive()
+			{
+				
+			}
+
+	}
