1ad48cbeae641112edb7ece113d1208a1ce62c11
Carlos J <cr.schoolwurk@gmail.com>
2018-09-19 23:22:54 -0600

    e5t7g4rhikow3q5utegwijo4r3wuy

--- DIGIT STRIPPING/src/DIGIT_STRIPPING_CHALLENGES.java
+++ DIGIT STRIPPING/src/DIGIT_STRIPPING_CHALLENGES.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class DIGIT_STRIPPING_CHALLENGES
 	{
+	
+		static int six[] = new int [6];
+		static int number;
+		static int numbers[];
+		static int sum = 0;
 
 		public static void main(String[] args)
 			{
-				challengeOne();
+				// challengeOne();
 				challengeTwo();
 				challengeThree();
 				challengeFour();
@@ -28,7 +33,27 @@ public class DIGIT_STRIPPING_CHALLENGES
 
 		private static void challengeTwo()
 			{
+				System.out.println("Give me any number of random numbers");
+				Scanner userNumbers = new Scanner(System.in);
+				number = userNumbers.nextInt();
+				int numberCount = number;
+				int counter = 0;
+				while (numberCount != 0)
+					{
+						numberCount = numberCount / 10;
+						counter ++;
+					
+					}
 				
+				if (counter == 1)
+					{
+						System.out.println("There is " + counter + " number in this list.");
+						
+					}
+				else
+					{
+						System.out.println("There are " + counter + " numbers in this list.");
+					}
 			}
 
 		private static void challengeThree()
